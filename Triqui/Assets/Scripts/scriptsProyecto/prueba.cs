db6250f367e8aeb00a06f129e78970505672eb91
Amaury06-19 <bulasalasamauryenrique@gmail.com>
2025-04-09 12:04:14 -0500

    XD

---
+++ Triqui/Assets/Scripts/scriptsProyecto/prueba.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+public class prueba
+{
+    public string nombre;
+    public int edad;
+
+    public prueba(string nombre, int edad){
+
+        this.nombre = nombre;
+        this.edad = edad;
+        
+    }
+}
